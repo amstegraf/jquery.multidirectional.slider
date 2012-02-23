73c4dff3cdc5e490460fc8f8e2422caa502fe20b
Aurelian Apostol <aurelian@codbug.com>
2012-02-23 02:16:14 +0200

    setting up the env for new jQuery plugin

---
+++ jquery.multidirectional.slider-0.1.0.js
@@ -0,0 +1,19 @@
+/**
+ * jquery.multidirectional.slider 1.0.0. A multidirectional slider plugin
+ * 
+ * Copyright (c) 2012 Aurelian Apostol
+ * http://codbug.com
+ *
+ * Licensed under GPLv3
+ * http://www.opensource.org/licenses/gpl-3.0.html
+ *
+ * Launch  : February 2012
+ * Version : 0.1.0
+ * Released: February 22, 2012 - 17:24pm
+ */
+
+(function($){
+	
+});
+
+
