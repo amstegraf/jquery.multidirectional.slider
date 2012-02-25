aeb87b9dc9dab1b0b3082d7abc053ba4944d9492
Aurelian Apostol <aurelian@codbug.com>
2012-02-25 20:22:18 +0200

    some patches added after testing in production

--- jquery.multidirectional.slider-0.3.1.js
+++ jquery.multidirectional.slider-0.3.1.js
@@ -8,62 +8,62 @@
  * http://www.opensource.org/licenses/gpl-3.0.html
  *
  * Launch  : February 2012
- * Version : 0.3.0
+ * Version : 0.3.1
  * Released: February 22, 2012 - 17:24pm
  */
 (function($){
 	function GoGoSlide(target, options){
 		var self = this;
 		target = $(target);
-	
+
 		$.extend(self, {
 			startSliding: function( ){
 				if (this.getInst(target)) {
 					return FALSE;
 				}
 				var inst = self.newInstance(target);
-				
+
 				self.setClipper();
 				self.setReel( options.orientation, options.width, options.height, options.margin );
 
 				$.data(target, "gogoslide", inst);
-				
+
 				target.children().hover(
-					function(){
-						self.stopSlider(target);
-					},
-					function(){
-						self.playSlider(target);
-					});
-				
+						function(){
+							self.stopSlider(target);
+						},
+						function(){
+							self.playSlider(target);
+						});
+
 				if (options.autoplay) {
 					self.playSlider(target);
 				}
-				
+
 			},
-			
+
 			setClipper: function(){
-				target.find(".clipper").css({width:options.width, height:options.height, 'overflow':'hidden', margin:options.margin});				
+				target.find(".clipper").css({width:options.width, height:options.height, 'overflow':'hidden', margin:options.margin, 'float':'left'});				
 			},
-			
+
 			setReel: function( orientation, width, height, margin ){
 				var clipperSum = target.find(".clipper").size();
 				switch( orientation ){
-					case 'horizontal':
-						target.css({width:options.frames*(width+margin*2),'overflow':'hidden'});
-						target.children().css({width:clipperSum*(width+margin*2)});
-						break;
-					case 'vertical':
-						target.css({height:options.frames*(height+margin*2), width:options.width+margin*2, 'overflow':'hidden'});
-						target.children().css({height:clipperSum*(height+margin*2)});
-						break;
+				case 'horizontal':
+					target.css({width:options.frames*(width+margin*2),'overflow':'hidden'});
+					target.children().css({width:clipperSum*(width+margin*2)});
+					break;
+				case 'vertical':
+					target.css({height:options.frames*(height+margin*2), width:options.width+margin*2, 'overflow':'hidden'});
+					target.children().css({height:clipperSum*(height+margin*2)});
+					break;
 				};
 			},
-			
+
 			playSlider: function( target ){
 				var inst = this.getInst(target);
 				if (!inst) return;
-				
+
 				inst.intervalID = window.setInterval(function () {
 					self.animate(target, self.nextSlider(target));
 					if (options.stopOnEnd && (inst.index === self.lastSlider(target)) ) {
@@ -72,85 +72,85 @@
 				}, options.speed+1000);
 				$.data(target, "gogoslide", inst);
 			},
-			
+
 			stopSlider: function (target) {
 				var inst = this.getInst(target);
 				if (!inst) return;
 				window.clearInterval(inst.intervalID);
 				$.data(target, "gogoslide", inst);
 			},
-			
+
 			animate: function (target, slider) {
 				var inst = this.getInst(target);
 				if(!inst) return;
-				
+
 				switch ( options.orientation ){
-					case 'horizontal':
-						if( options.type === 'jump' ) var speed = 500;
-						else var speed = options.speed;
-						
-						target.children("div").animate({marginLeft: slider}, speed, 'swing', function(){});
-						break;
-					case 'vertical':
-						if( options.type === 'jump' ) var speed = 500;
-						else var speed = options.speed;
-						
-						target.children("div").animate({marginTop: slider}, speed, 'swing', function(){});
-						break;
+				case 'horizontal':
+					if( options.type === 'jump' ) var speed = 500;
+					else var speed = options.speed;
+
+					target.children("div").animate({marginLeft: slider}, speed, 'swing', function(){});
+					break;
+				case 'vertical':
+					if( options.type === 'jump' ) var speed = 500;
+					else var speed = options.speed;
+
+					target.children("div").animate({marginTop: slider}, speed, 'swing', function(){});
+					break;
 				};
 			},
-			
+
 			firstSlider: function (target) {				
 				return 1;
-	        },
-	        
-	        lastSlider: function (target) {				
-	        	var lastSlider = ( target.find(".clipper").size() / options.frames );
-	        	
-	        	return lastSlider;
-	        },
-			
+			},
+
+			lastSlider: function (target) {				
+				var lastSlider = Math.round( target.find(".clipper").size() / options.frames );
+
+				return lastSlider;
+			},
+
 			prevSlider: function (target) {
 				var inst = this.getInst(target);
 				if(!inst) return;
-				
-				var previousPosition = (inst.index-1)*self.getViewport();
+
+				var previousPosition = (inst.index-1)*self.getViewport(target);
 				inst.index--;
-				
+
 				return previousPosition;
 			},
-			
+
 			nextSlider: function (target) {
 				var inst = this.getInst(target);
 				if(!inst) return;
-				
-				var nextPosition = -( inst.index*self.getViewport() );
+
+				var nextPosition = -( inst.index*self.getViewport(target) );
 				inst.index++;
-				
+
 				return nextPosition;
-				
+
 			},
-			
-			getViewport: function(){
+
+			getViewport: function(target){
 				switch(options.orientation){
-					case 'horizontal':
-						viewportSize = (options.width+options.margin*2)*options.frames;
-						break;
-					case 'vertical':
-						viewportSize = (options.height+options.margin*2)*options.frames;
-						break;
+				case 'horizontal':
+					viewportSize = (options.width+options.margin*2)*options.frames;
+					break;
+				case 'vertical':
+					viewportSize = (options.height+options.margin*2)*options.frames;
+					break;
 				}
-				
+
 				return viewportSize;
 			},
-			
+
 			goTo: function(slider){
 				var inst = this.getInst(target);
 				if(!inst) return;
-				
+
 				inst.index = slider-1;
 			},
-			
+
 			newInstance: function(target) {
 				var id = target[0].id.replace(/([^A-Za-z0-9_-])/g, '\\\\$1');
 				return {
@@ -170,14 +170,14 @@
 					throw 'Missing instance data for this slider';
 				}
 			},
-			
+
 			get: function(inst, name) {
 				return inst.options[name] !== undefined ? inst.options[name] : this.defaults[name];
 			}
 		});
-		
+
 	};
-	
+
 	$.fn.gogoslide = function( options ){
 		var _defaults = $.extend({
 			//Play settings
@@ -186,11 +186,7 @@
 			speed: 2000,
 			focus: 0,
 			stopOnEnd: false,
-			type: 'slide', //jump
-			onStart: null,
-			onEnd: null,
-			onFirst: null,
-			onLast: null,
+			type: 'slide', // SLIDE OR JUMP
 			
 			//Display settings
 			width: 193,
@@ -198,15 +194,15 @@
 			margin: 0,
 			orientation: 'horizontal', //vertical
 			anchors: false,
-			
+
 			//Debug Mode
 			debug: false
 		}, options);
-		
+
 		return this.each(function(){
 			$.gogoslide = new GoGoSlide(this, _defaults);
 			$.gogoslide.startSliding();
 		});
 	};
-	
+
 })(jQuery);
